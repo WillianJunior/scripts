602077108028e6bd5f7cfe2e839e8d90f2583bb5
Willian <guns945@gmail.com>
2020-02-03 14:57:52 -0300

    added error and repetitions to script

--- disTiler/bridgesTilerRegression.sh
+++ disTiler/bridgesTilerRegression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -p RM
-#SBATCH --ntasks-per-node 4
+#SBATCH --ntasks-per-node 28
 #SBATCH -t 4:00:00
 
 set -x
@@ -14,4 +14,4 @@ cd $LOCAL
 
 module load cmake/3.11.4 cuda/10.1 gcc/6.3.0 openslide/3.4.1 mpi/gcc_openmpi
 
-bash tilerTimeXcostXiterations.sh IMAGES_ZIP
+bash tilerTimeXcostXiterations.sh $IMAGES_ZIP
--- disTiler/tilerTimeXcostXiterations.sh
+++ disTiler/tilerTimeXcostXiterations.sh
@@ -20,12 +20,16 @@ else
     EXEC=1
 fi
 
-THRS=( 50 100 150 200 )
-ERODE=( 2 4 8 10 )
-DILATE=( 2 4 8 10 )
+THRS=( 100 150 )
+ERODE=( 2 )
+DILATE=( 2 )
+# ERODE=( 2 4 8 10 )
+# DILATE=( 2 4 8 10 )
+
+REPS=3
 
 echo "thrs/erode/dilate"
-echo -n "img time it "
+echo -n "img time time_error it "
 for (( T=0; T<${#THRS[@]}; T++ )); do
     for (( E=0; E<${#ERODE[@]}; E++ )); do
         for (( D=0; D<${#DILATE[@]}; D++ )); do
@@ -39,23 +43,43 @@ for I in ${IMGS[@]}; do
     # initial test: exectime and iterations
     echo -n "$(basename $I) "
     if [[ $EXEC == 1 ]]; then
-        OUT=$(mpirun -np 2 ./iwpp ${I} -a 0)
-        # echo "$OUT"
-        CUR_TIME=$(grep FULL_TIME <<< "$OUT" | awk -e '{print $2}')
+        SUM_TIME=0
+        ERR_LIST=()
+        for (( R=0; R<REPS; R++ )); do
+            OUT=$(mpirun -np 2 ./iwpp ${I} -a 0)
+            # echo "$OUT"
+            CUR_TIME=$(grep FULL_TIME <<< "$OUT" | awk -e '{print $2}')
 
-        # get max iteration num
-        ITS=$(grep iterations <<< "$OUT" | awk -e '{print $3}')
-        ITSCOUNT=$(awk -v c=0 '{c++} END {print c}' <<< "$ITS")
-        if [[ ITSCOUNT == 0 ]]; then
-            IT=0
-        else
-            IT=$(awk -v max=0 '{if($1>max){max=$1}}END{print max} ' <<< "$ITS")
-        fi
+            SUM_TIME=$(($SUM_TIME + $CUR_TIME))
+            # ERR_LIST=$ERR_LIST$CUR_TIME$'\n'
+            ERR_LIST[${R}]=$CUR_TIME
+
+            # get max iteration num
+            ITS=$(grep iterations <<< "$OUT" | awk -e '{print $3}')
+            ITSCOUNT=$(awk -v c=0 '{c++} END {print c}' <<< "$ITS")
+            if [[ ITSCOUNT == 0 ]]; then
+                IT=0
+            else
+                IT=$(awk -v max=0 '{if($1>max){max=$1}}END{print max} ' <<< "$ITS")
+            fi
+        done
+        CUR_TIME=$(($SUM_TIME/$REPS))
+
+        # calculate mesure error
+        # ERR=$(awk -v errs=0 -v AVRG=$CUR_TIME '{errs += bc <<< "sqrt(pow($1-AVRG,2))"; c++} END {print total/c c}' <<< "$ERR_LIST")
+
+        SUM_ERR=0
+        for (( E=0; E<${#ERR_LIST[@]}; E++ )); do
+            ERR=$(bc <<< "sqrt((${ERR_LIST[${E}]}-${CUR_TIME})^2)")
+            ERR_LIST2[${E}]=$ERR_LIST2$ERR$'\n'
+            SUM_ERR=$(($SUM_ERR+$ERR))
+        done
     else
         CUR_TIME=0
+        SUM_ERR=0
         IT=0
     fi
-    echo -n $CUR_TIME $IT" "
+    echo -n $CUR_TIME $SUM_ERR $IT" "
 
     # get cost for each parameters set
     for (( T=0; T<${#THRS[@]}; T++ )); do
